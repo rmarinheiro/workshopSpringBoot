8ac4c7e41d39d80f254b5df21319aaa5baf1fe0b
= <=>
2019-05-28 12:39:03 -0300

    Projeto Criado

---
+++ src/main/java/com/rafael/cursomc/WorkShopSpringApplication.java
@@ -0,0 +1,13 @@
+package com.rafael.cursomc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkShopSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WorkShopSpringApplication.class, args);
+	}
+
+}
